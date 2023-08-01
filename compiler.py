8b9920b359b3d59db5222af731ae1db27f2397f8
Cem Aydin <cem.aydin@gmx.ch>
2023-08-01 17:52:05 +0200

    restructure

--- compiler.py
+++ compiler.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# toy compiler
+# a toy compiler for a minimal toy language
 #
 
 import sys
@@ -9,14 +9,6 @@ from assembly import LITERAL, PRIMARIES, UNARIES, BINARIES, \
   HEAD, START, EXIT, DEFAULT_EXIT, PRINTCHAR, PRINT
 
 OUTFILE = 'out.asm'
-
-if len(sys.argv) < 2:
-  sys.exit("""Usage: compiler.py <program file>
-Output: {}""".format(OUTFILE))
-
-# open program file
-with open(sys.argv[1], 'r') as f:
-  PROGRAM = f.read()
 
 def collapse_expressions(program):
 
@@ -35,7 +27,11 @@ def collapse_expressions(program):
 
   return collapsed_program
 
+
 def split_args(argstr):
+
+  ### helper function to split comma-separated arguments
+
   args = []
   arg = ''
   count = 0
@@ -54,82 +50,104 @@ def split_args(argstr):
   args.append(arg.strip())
   return args
 
-# intermediate representation
-REPR = ""
 
-def parse_expression(expr):
-  global REPR
+def parse_expression(expr, repr):
+
+  ### parse an expression and return an intermediate representation
+
   # trim whitespace
   expr = expr.strip()
 
   # check for literal
   if expr.isdigit():
-    REPR += 'LITERAL ' + expr + '\n'
-    return
+    repr += 'LITERAL ' + expr + '\n'
+    return repr
 
   kw = expr.split('(', 1)[0]
 
   # check for primary
   if kw in PRIMARIES:
-    REPR += 'PRIMARY ' + kw + '\n'
-    return
+    repr += 'PRIMARY ' + kw + '\n'
+    return repr
 
   # check for unary
   if kw in UNARIES:
     arg = expr.split('(', 1)[1][:-1]
     if kw == 'exit' and arg == '':
-      parse_expression('0')
+      repr = parse_expression('0', repr)
     else:
-      parse_expression(arg)
-    REPR += 'UNARY ' + kw + '\n'
-    return
+      repr = parse_expression(arg, repr)
+    repr += 'UNARY ' + kw + '\n'
+    return repr
 
   # check for binary
   if kw in BINARIES:
     argstr = expr.split('(', 1)[1][:-1]
     args = split_args(argstr)
-    parse_expression(args[0])
-    parse_expression(args[1])
-    REPR += 'BINARY ' + kw + '\n'
-    return
+    repr = parse_expression(args[0], repr)
+    repr = parse_expression(args[1], repr)
+    repr += 'BINARY ' + kw + '\n'
+    return repr
 
   sys.exit("Unknown keyword: '" + kw + "'")
 
 
-# preprocess program
-COLLAPSED_PROGRAM = collapse_expressions(PROGRAM)
-
-for line in COLLAPSED_PROGRAM.splitlines():
-  line = line.strip()
-  if line == '':
-    continue
-  if line[0] == '#':
-    continue
-  parse_expression(line)
-
-# assembly code
-ASM = ""
-
 def emit(expr):
-  global ASM
+
+  ### emit assembly code from intermediate representation
+
   [ optype, opval ] = expr.split(' ', 1)
   if optype == 'LITERAL':
-    ASM += LITERAL.format(opval)
-  elif optype == 'PRIMARY':
-    ASM += PRIMARIES[opval]
-  elif optype == 'UNARY':
-    ASM += UNARIES[opval]
-  elif optype == 'BINARY':
-    ASM += BINARIES[opval]
-  else:
-    sys.exit("Erroneous intermediate expression: " + expr)
+    return LITERAL.format(opval)
+  if optype == 'PRIMARY':
+    return PRIMARIES[opval]
+  if optype == 'UNARY':
+    return UNARIES[opval]
+  if optype == 'BINARY':
+    return BINARIES[opval]
+  sys.exit("Erroneous intermediate expression: " + expr)
 
 
-for expr in REPR.splitlines():
-  emit(expr)
+def main():
 
-OUT = HEAD + EXIT + PRINTCHAR + PRINT + \
-  START + ASM + DEFAULT_EXIT
+  ### main function
 
-with open(OUTFILE, 'w') as f:
-  f.write(OUT)
+  # check for program file
+  if len(sys.argv) < 2:
+    sys.exit("""Usage: compiler.py <program file>
+  Output: {}""".format(OUTFILE))
+
+  # open program file
+  with open(sys.argv[1], 'r') as f:
+    program = f.read()
+
+  # preprocess program
+  collapsed_program = collapse_expressions(program)
+
+  # parse expressions into intermediate representation
+  intermediate_repr = ''
+  for line in collapsed_program.splitlines():
+    line = line.strip()
+    if line == '':
+      continue
+    if line[0] == '#':
+      continue
+    intermediate_repr += parse_expression(line, '')
+
+  # emit assembly code from intermediate representation
+  main_asm = ''
+  for expr in intermediate_repr.splitlines():
+    main_asm += emit(expr)
+
+  # combine main assembly code with header, built-in functions and footer
+  out = HEAD + EXIT + PRINTCHAR + PRINT + \
+    START + main_asm + DEFAULT_EXIT
+
+  # write to output file
+  with open(OUTFILE, 'w') as f:
+    f.write(out)
+
+
+# run main function
+if __name__ == '__main__':
+  main()
