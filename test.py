a4dfc73c915d279b7edc21fe1d1519ef42323541
Cem Aydin <cem.aydin@gmx.ch>
2023-09-17 13:53:35 +0200

    function-param-type  ext

--- test.py
+++ test.py
@@ -56,7 +56,7 @@ CASES = [
   [ 'programs/40-string-lit-loop.src', 'AAAAA', 0 ],
   [ 'programs/41-fizzbuzz.src', '1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\n11\nfizz\n13\n14\nfizzbuzz\n', 0 ],
   [ 'programs/42-int_to_str.src', '123', 0 ],
-  [ 'programs/43-function-param-type.src', '123ABC', 0 ],
+  [ 'programs/43-function-param-type.src', '123ABC456', 0 ],
 ]
 
 for case in CASES:
